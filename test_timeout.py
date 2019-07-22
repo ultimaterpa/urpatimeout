3b29cc5608ced441ca62e1f45c24f5cadce74496
Slavaqq <slavaqq@gmail.com>
2019-07-22 14:50:31 +0200

    Add more test cases

--- test_timeout.py
+++ test_timeout.py
@@ -5,29 +5,37 @@ import pytest
 from timeout import Timeout
 
 
-ARG = "timeout_ms,first_check_s,second_check_s"
-VALUES = ((10, 0.005, 0.006), (1, 0.0005, 0.0011), (100, 0.05, 0.06), (1000, 0.5, 0.6))
+TIMEOUTS_MS = (2, 10, 50, 100, 200, 1000, 1500, 5000, 20000)
 
 
-@pytest.mark.parametrize(ARG, VALUES)
-def test_remaining(timeout_ms, first_check_s, second_check_s):
-    t = Timeout(timeout_ms)
+@pytest.fixture(params=TIMEOUTS_MS)
+def timeout_ms(request):
+    return request.param, request.param // 2 / 1000, request.param * 1.1 / 1000
+
+
+def test_remaining(timeout_ms):
+    max_ms, first_check_s, second_check_s = timeout_ms
+    t = Timeout(max_ms)
     time.sleep(first_check_s)
     assert t.remaining() > 0
     time.sleep(second_check_s)
     assert t.remaining() < 0
 
 
-@pytest.mark.parametrize(ARG, VALUES)
-def test_is_expired(timeout_ms, first_check_s, second_check_s):
-    t = Timeout(timeout_ms)
+def test_is_expired(timeout_ms):
+    max_ms, first_check_s, second_check_s = timeout_ms
+    t = Timeout(max_ms)
     time.sleep(first_check_s)
     assert t.is_expired() == False
     time.sleep(second_check_s)
-    assert t.is_expired() == True
+    assert t.is_expired()
 
 
-def test_elapsed():
-    t = Timeout(10000)
-    time.sleep(0.005)
-    assert t.elapsed() >= 5 and t.elapsed() < 7
+@pytest.mark.parametrize("more_ms", (5, 20, 40, 300, 1000, 5000, 20000))
+def test_elapsed(more_ms):
+    delay_s = more_ms / 1000
+    less_ms = more_ms + 5
+    t = Timeout(1e10)
+    time.sleep(delay_s)
+    elapsed = t.elapsed()
+    assert elapsed >= more_ms and elapsed <= less_ms
