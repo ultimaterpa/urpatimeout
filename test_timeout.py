79f65c84cdb17552e24d76c37fc8c69c9beb4cf5
Slavaqq <slavaqq@gmail.com>
2019-07-24 08:54:06 +0200

    Tests for type checks and exception.

--- test_timeout.py
+++ test_timeout.py
@@ -10,23 +10,28 @@ TIMEOUTS_MS = (2, 10, 50, 100, 200, 1000, 1500, 5000, 20000)
 
 @pytest.fixture(params=TIMEOUTS_MS)
 def timeout_ms(request):
+    return request.param
+
+
+@pytest.fixture(params=TIMEOUTS_MS)
+def timeout_checks(request):
     return request.param, request.param // 2 / 1000, request.param * 1.1 / 1000
 
 
-def test_remaining(timeout_ms):
-    max_ms, first_check_s, second_check_s = timeout_ms
-    t = Timeout(max_ms)
+def test_remaining(timeout_checks):
+    timeout_ms, first_check_s, second_check_s = timeout_checks
+    t = Timeout(timeout_ms)
     time.sleep(first_check_s)
     assert t.remaining() > 0
     time.sleep(second_check_s)
     assert t.remaining() < 0
 
 
-def test_is_expired(timeout_ms):
-    max_ms, first_check_s, second_check_s = timeout_ms
-    t = Timeout(max_ms)
+def test_is_expired(timeout_checks):
+    timeout_ms, first_check_s, second_check_s = timeout_checks
+    t = Timeout(timeout_ms)
     time.sleep(first_check_s)
-    assert t.is_expired() == False
+    assert t.is_expired() is False
     time.sleep(second_check_s)
     assert t.is_expired()
 
@@ -35,7 +40,23 @@ def test_is_expired(timeout_ms):
 def test_elapsed(more_ms):
     delay_s = more_ms / 1000
     less_ms = more_ms + 5
-    t = Timeout(1e10)
+    t = Timeout(1000000)
     time.sleep(delay_s)
     elapsed = t.elapsed()
     assert elapsed >= more_ms and elapsed <= less_ms
+
+
+@pytest.mark.parametrize("wrong_timeout", (-3.1, -1, 0.0, 0, 1.0, 1e10))
+def test_init(wrong_timeout):
+    with pytest.raises(TypeError):
+        Timeout(wrong_timeout)
+
+
+def test_remaining_type(timeout_ms):
+    t = Timeout(timeout_ms)
+    assert type(t.remaining()) is int
+
+
+def test_elapsed_type(timeout_ms):
+    t = Timeout(timeout_ms)
+    assert type(t.elapsed()) is int
