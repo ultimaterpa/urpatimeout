eb0a8c579203459ea0a60b688d8551144ece6c39
anerold <adam.nerold@seznam.cz>
2021-04-30 08:54:11 +0200

    Added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(
     packages=["urpatimeout"],
     python_requires=">=3.7",
     classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: Microsoft :: Windows",
         "License :: OSI Approved :: MIT License",
     ],
     zip_safe=False,
